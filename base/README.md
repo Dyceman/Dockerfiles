86d364f762f848e6008c8991e174e63fc95dfb7a
Michael (Parker) Parker <parkervcp@gmail.com>
2017-02-09 10:05:49 -0500

    adding base files for others

---
+++ base/README.md
@@ -0,0 +1,2 @@
+# Don't use these yet
+These are just base files that have no filler in them.
---
+++ base/alpine/start.sh
@@ -0,0 +1,6 @@
+#!/bin/ash
+sleep 5
+
+# Insert bash script to setup the server here
+
+# Insert stuff to start the game here
